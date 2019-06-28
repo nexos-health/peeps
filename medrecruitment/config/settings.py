c280e899bf1b3b06ebe1678bf4dcfe09fe42db8a
Elliott Lovell <elliott@repositpower.com>
2019-06-28 21:56:21 +1000

    changed allowed hosts

--- medrecruitment/config/settings.py
+++ medrecruitment/config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '!gl%pc09wt%jwpi-tdawljpufm4sqerjno4n4^)*75jx8g4+hi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
