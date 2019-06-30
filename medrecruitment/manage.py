22304341c21756787ac5c0212373706b5d4c23bb
Elliott Lovell <elliott@repositpower.com>
2019-06-30 15:41:55 +1000

    changed .env filepath

--- medrecruitment/manage.py
+++ medrecruitment/manage.py
@@ -5,7 +5,7 @@ import sys
 import dotenv
 
 if __name__ == '__main__':
-    dotenv.read_dotenv("../.env")
+    dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"))
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
