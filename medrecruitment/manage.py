53e4b4b20faec7ebb3322faf8a0447bc7fb3f1c7
Elliott Lovell <elliott@repositpower.com>
2019-06-30 15:58:58 +1000

    fixed now?

--- medrecruitment/manage.py
+++ medrecruitment/manage.py
@@ -6,7 +6,7 @@ import dotenv
 
 if __name__ == '__main__':
     dotenv.read_dotenv(
-        os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env")
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env")
     )
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
