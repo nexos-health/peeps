64910e72c5a85b56e8ea7bf290c64087ff5014b1
Elliott Lovell <elliott@repositpower.com>
2020-10-25 22:55:19 +1100

    added get_favourites route

--- medrecruitment/professionals/views.py
+++ medrecruitment/professionals/views.py
@@ -152,6 +152,14 @@ class ProfessionalGroupsViewSet(viewsets.ModelViewSet):
 
         return Response(data=groups_dict)
 
+    @action(detail=False, methods=["GET"])
+    def get_favourites(self, request, *args, **kwargs):
+        user_key = request.query_params.get("user_key")
+        groups = ProfessionalGroup.objects.filter(user__user_key=user_key, name="Favourites")
+        groups_dict = format_professional_groups(groups)
+
+        return Response(data=groups_dict)
+
     @action(detail=False, methods=["PUT"])
     def edit_group(self, request, *args, **kwargs):
         user_key = request.data.get("user_key")
@@ -210,9 +218,9 @@ class ProfessionalGroupsViewSet(viewsets.ModelViewSet):
             ProfessionalGroupMapping.objects.bulk_create(group_mappings)
             group_dict = format_professional_groups([group])
         except ProfessionalGroup.DoesNotExist:
-            return Response(f"Group with user_key: {user_key}, group_uid:{group_uid} was not found.", status=404)
+            return Response(f"Group with user_key: {user_key}, group_uid:{group_uid} was not found.", status=400)
         except Professional.DoesNotExist:
-            return Response(f"Professional with professional_uids: {professional_uids} were not found.", status=404)
+            return Response(f"Professional with professional_uids: {professional_uids} were not found.", status=400)
         except Exception as ex:
             return Response(f"{ex}"), 500
         else:
@@ -238,11 +246,11 @@ class ProfessionalGroupsViewSet(viewsets.ModelViewSet):
             group_dict = format_professional_groups([group])
         except ProfessionalGroup.DoesNotExist:
             return Response(
-                f"Group with user_key: {user_key}, group_uid:{group_uid} was not found.", status=404
+                f"Group with user_key: {user_key}, group_uid:{group_uid} was not found.", status=400
             )
         except Professional.DoesNotExist:
             return Response(
-                f"Professionals with professional_uids: {professional_uids} were not found.", status=404
+                f"Professionals with professional_uids: {professional_uids} were not found.", status=400
             )
         except Exception as ex:
             return Response(f"{ex}"), 500
